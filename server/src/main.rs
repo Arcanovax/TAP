f1838de7a3bbf325770e6fcec7837f64cb2df43b
relaforg <remi.laforgue@gmail.com>
2026-06-02 15:11:58 +0200

    Server setup

---
+++ server/src/main.rs
@@ -0,0 +1,40 @@
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+
+        if let Err(_e) = socket
+            .write_all("Server → Client: OK hello proto=1\n".as_bytes())
+            .await
+        {
+            eprintln!("failed to connect establish TCP connection with server");
+        }
+        tokio::spawn(async move {
+            let mut buf = [0; 1024];
+
+            // In a loop, read data from the socket and write the data back.
+            loop {
+                let n = match socket.read(&mut buf).await {
+                    // socket closed
+                    Ok(0) => return,
+                    Ok(n) => n,
+                    Err(e) => {
+                        eprintln!("failed to read from socket; err = {:?}", e);
+                        return;
+                    }
+                };
+
+                // Write the data back
+                if let Err(e) = socket.write_all(&buf[0..n]).await {
+                    eprintln!("failed to write to socket; err = {:?}", e);
+                    return;
+                }
+            }
+        });
+    }
+}
