8e6749b2d85709441356b84e22c5ad96418b7f0c
relaforg <remi.laforgue@gmail.com>
2026-06-16 10:01:55 +0200

    Fix who return value

--- server/src/handlers/who.rs
+++ server/src/handlers/who.rs
@@ -1,3 +1,5 @@
+use serde_json::json;
+
 use crate::error::ErrorCode;
 use crate::protocol::Message;
 use crate::state::SharedServer;
@@ -8,8 +10,6 @@ mod tests;
 pub(super) fn who_request(server_info: &SharedServer) -> Message {
     Message::Response {
         error: ErrorCode::SUCCESS,
-        data: Some(
-            serde_json::to_value(server_info.lock().unwrap().get_number_of_players()).unwrap(),
-        ),
+        data: Some(json!({ "players": server_info.lock().unwrap().get_number_of_players() })),
     }
 }
