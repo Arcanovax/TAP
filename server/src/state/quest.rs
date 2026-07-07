d963eb69c9a1558bd29a0a1590f95f2125ac2f24
relaforg <laforgueremi@gmail.com>
2026-07-07 13:11:47 +0200

    revert

--- server/src/state/quest.rs
+++ server/src/state/quest.rs
@@ -78,7 +78,7 @@ impl ServerInfo {
                     player.quests_in_progress.remove(id);
                 }
             } else {
-                send_quest_update_event(id.to_string(), &quest, new_step - 1, tx);
+                send_quest_update_event(id.to_string(), &quest, new_step, tx);
             }
         }
     }
