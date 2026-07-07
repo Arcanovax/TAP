fbf4d2f751aa5eec8b4b253043bc2d848f6301a1
Arcanovax <t.matisse2007@gmail.com>
2026-07-07 18:18:57 +0200

    refacto sell and buy button

--- src/npc.rs
+++ src/npc.rs
@@ -190,65 +190,36 @@ fn handle_shop(game: &mut Game, npc: &Npc){
 					22.5,
 					WHITE,
 				);
-				let btn_buy = Rect::new(shop_rect.x + shop_rect.w - 115.0, line + (50.0 - item_size) / 2.0, 50.0,37.5);
+
+				let btn_buy = Rect::new(shop_rect.x + shop_rect.w - 115.0,line + (50.0 - item_size) / 2.0, 50.0,37.5);
 				if let Some(info) = game.npc_shop.buy_info.as_ref() {
 					if get_time() - info.time > 0.5 {
 						game.npc_shop.buy_info = None;
 					}
-					else if btn_buy.contains(game.mouse){
-						let color = info.color;
-						if get_button(btn_buy, "Buy", 25, color ,game.mouse) && !game.group.typed.is_empty(){
-							let rq: String = format!("BUY {} {} \n",npc.id,item);
-							game.tx_to_serv.try_send(rq).ok();
-							game.pending_action = PendingAction::Buy(item.to_string());
-						}
-					}
-					else{
-						if get_button(btn_buy, "Buy", 25, WHITE, game.mouse){
-							let rq: String = format!("BUY {} {} \n",npc.id,item);
-							game.tx_to_serv.try_send(rq).ok();
-							game.pending_action = PendingAction::Buy(item.to_string());
-						}
-					}
-
 				}
-				else{
-					if get_button(btn_buy, "Buy", 25, WHITE, game.mouse){
-							let rq: String = format!("BUY {} {} \n",npc.id,item);
-							game.tx_to_serv.try_send(rq).ok();
-							game.pending_action = PendingAction::Buy(item.to_string());
-					}
+				let info = game.npc_shop.buy_info.as_ref().filter(|_| btn_buy.contains(game.mouse));
+				let color = info.map(|info| info.color).unwrap_or(WHITE);
+				let allowed = info.is_none();
+				if get_button(btn_buy, "Buy", 25, color, game.mouse) && allowed {
+					let rq = format!("BUY {} {} \n", npc.id, item);
+					game.tx_to_serv.try_send(rq).ok();
+					game.pending_action = PendingAction::Buy(item.to_string());
 				}
-
 
 				let btn_sell = Rect::new(shop_rect.x + shop_rect.w - 60.0, line + (50.0 - item_size) / 2.0, 50.0,37.5);
 				if let Some(info) = game.npc_shop.sell_info.as_ref() {
 					if get_time() - info.time > 0.5 {
 						game.npc_shop.sell_info = None;
 					}
-					else if btn_sell.contains(game.mouse){
-						let color = info.color;
-						if get_button(btn_sell, "Sell", 25, color,game.mouse) && !game.group.typed.is_empty(){
-							let rq: String = format!("SELL {} {} \n",npc.id,item);
-							game.tx_to_serv.try_send(rq).ok();
-							game.pending_action = PendingAction::Sell(item.to_string());
-						}
-					}
-					else{
-						if get_button(btn_sell, "Sell", 25, WHITE, game.mouse){
-							let rq: String = format!("SELL {} {} \n",npc.id,item);
-							game.tx_to_serv.try_send(rq).ok();
-							game.pending_action = PendingAction::Sell(item.to_string());
-						}
-					}
-
 				}
-				else{
-					if get_button(btn_sell, "Sell", 25, WHITE, game.mouse){
-							let rq: String = format!("SELL {} {} \n",npc.id,item);
-							game.tx_to_serv.try_send(rq).ok();
-							game.pending_action = PendingAction::Sell(item.to_string());
-				}}
+				let info = game.npc_shop.sell_info.as_ref().filter(|_| btn_sell.contains(game.mouse));
+				let color = info.map(|info| info.color).unwrap_or(WHITE);
+				let allowed = info.is_none();
+				if get_button(btn_sell, "Sell", 25, color, game.mouse) && allowed {
+					let rq = format!("SELL {} {} \n", npc.id, item);
+					game.tx_to_serv.try_send(rq).ok();
+					game.pending_action = PendingAction::Sell(item.to_string());
+				}
 
 			}
 		}
